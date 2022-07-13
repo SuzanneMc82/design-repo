3890d357f557234081aa21cf889c43b94e5b9499
Matthew Jack <matthew.jack@sds.co.uk>
2022-07-13 10:33:22 +0100

    added init comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { SimpleTextComponent } from './simple-text/simple-text.component';
+import { ButtonListComponent } from './button-list/button-list.component';
+import { InfoListComponent } from './info-list/info-list.component';
+import { CardComponent } from './card/card.component';
+import { InfoPanelComponent } from './info-panel/info-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TabsComponent,
+    SimpleTextComponent,
+    ButtonListComponent,
+    InfoListComponent,
+    CardComponent,
+    InfoPanelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/simple-text/simple-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-text',
+  templateUrl: './simple-text.component.html',
+  styleUrls: ['./simple-text.component.css']
+})
+export class SimpleTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
